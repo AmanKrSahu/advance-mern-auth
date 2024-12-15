6c08e868d393098c30ad51dfde294f764ca364af
Aman Sahu <92854825+AmanKrSahu@users.noreply.github.com>
2024-12-15 21:24:15 +0530

    feat: Added Login API (#4)

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express"
 import { HTTPSTATUS } from "../../config/http.config"
-import { registerSchema } from "../../common/validators/auth.validator"
+import {
+  loginSchema,
+  registerSchema,
+} from "../../common/validators/auth.validator"
+import { setAuthenticationCookies } from "../../common/utils/cookie"
 
 import { asyncHandler } from "../../middlewares/asyncHandler"
 
@@ -25,6 +29,32 @@ export class AuthController {
         message: "User registered successfully",
         data: user,
       })
+    }
+  )
+
+  public login = asyncHandler(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async (req: Request, res: Response): Promise<any> => {
+      const userAgent = req.headers["user-agent"]
+      const body = loginSchema.parse({
+        ...req.body,
+        userAgent,
+      })
+
+      const { user, accessToken, refreshToken, mfaRequired } =
+        await this.authService.login(body)
+
+      return setAuthenticationCookies({
+        res,
+        accessToken,
+        refreshToken,
+      })
+        .status(HTTPSTATUS.OK)
+        .json({
+          message: "User login successfully",
+          mfaRequired,
+          user,
+        })
     }
   )
 }
