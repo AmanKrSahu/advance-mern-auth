5006a14006a4bdc501a489d5352952e5b5ba3bdb
Aman <amankrsahu003@gmail.com>
2024-12-21 17:21:53 +0530

    feat: Added Refresh API

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from "express"
+
 import { HTTPSTATUS } from "../../config/http.config"
 import {
   loginSchema,
   registerSchema,
 } from "../../common/validators/auth.validator"
-import { setAuthenticationCookies } from "../../common/utils/cookie"
+import { UnauthorizedException } from "../../common/utils/catch-errors"
+import {
+  getAccessTokenCookieOptions,
+  getRefreshTokenCookieOptions,
+  setAuthenticationCookies,
+} from "../../common/utils/cookie"
 
 import { asyncHandler } from "../../middlewares/asyncHandler"
 
@@ -54,6 +60,35 @@ export class AuthController {
           message: "User login successfully",
           mfaRequired,
           user,
+        })
+    }
+  )
+
+  public refreshToken = asyncHandler(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async (req: Request, res: Response): Promise<any> => {
+      const refreshToken = req.cookies.refreshToken as string | undefined
+
+      if (!refreshToken) {
+        throw new UnauthorizedException("Missing refresh token")
+      }
+
+      const { accessToken, newRefreshToken } =
+        await this.authService.refreshToken(refreshToken)
+
+      if (newRefreshToken) {
+        res.cookie(
+          "refreshToken",
+          newRefreshToken,
+          getRefreshTokenCookieOptions()
+        )
+      }
+
+      return res
+        .status(HTTPSTATUS.OK)
+        .cookie("accessToken", accessToken, getAccessTokenCookieOptions())
+        .json({
+          message: "Refresh access token successfully",
         })
     }
   )
